a9bcfec0ac2396d1330e794c9a7e5b8a68927e86
kimim <kimim@KRASIMIRKATA>
2023-01-04 19:49:51 +0200

    add three buttons, Rectangle and Circle

--- Figures/Form1.Designer.cs
+++ Figures/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btnRectangle.Location = new System.Drawing.Point(51, 383);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(123, 34);
+            this.btnRectangle.TabIndex = 0;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btnTriangle.Location = new System.Drawing.Point(324, 383);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(111, 34);
+            this.btnTriangle.TabIndex = 1;
+            this.btnTriangle.Text = "Triangle";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btnCircle.Location = new System.Drawing.Point(622, 383);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(90, 34);
+            this.btnCircle.TabIndex = 2;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnTriangle);
+            this.Controls.Add(this.btnRectangle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnCircle;
     }
 }
 
--- Figures/Form1.cs
+++ Figures/Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Figures
 {
@@ -15,6 +17,63 @@ namespace Figures
         public Form1()
         {
             InitializeComponent();
+        }
+        Random rdm;
+
+        public void threadTriangle()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int width = rdm.Next(0, 550);
+                int height = rdm.Next(0, 350);
+                Thread.Sleep(3000);
+            }
+        }
+
+        private void btnTriangle_Click(object sender, EventArgs e)
+        {
+            Thread thTriangle = new Thread(threadTriangle);
+            thTriangle.Start();
+        }
+
+        public void threadRectangle()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int width = rdm.Next(0, 550);
+                int height = rdm.Next(0, 300);
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Black, 5), width, height, 50, 50);
+                Thread.Sleep(3000);
+            }
+        }
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+          Thread thRectangle = new Thread(threadRectangle);
+            thRectangle.Start(); 
+        }
+     
+        public void threadCircle()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int width = rdm.Next(0, 550);
+                int height = rdm.Next(0, 300);
+                this.CreateGraphics().DrawEllipse(new Pen(Brushes.Green, 8), width, height, 50, 50);
+                Thread.Sleep(4000);
+               
+            }
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            Thread thCircle = new Thread(threadCircle);
+            thCircle.Start();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdm = new Random();
         }
     }
 }
